44d581178a718b50d8057ea50a5a3ae8f8cf7b6b
Devin Wilson <craigvin84@yahoo.com>
2020-10-31 20:59:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,15 +4,33 @@ class School
 
 roster => {}
 
-def add_student=(name, grade)
-  @name=(name)
-  @grade=(grade)
-  roster[grade] << name
+def initialize(name)
+  @name = name
+  @roster = {}
 end
 
-def add_student
-  @name
-  @grade
+
+def add_student=(student, level)
+  roster[level] ||= []
+  roster[level] << student
 end
+
+def grade(level)
+  roster.detect do |x, y|
+    if x == level
+      return y 
+    end
+  end
+end
+
+def sort
+  nu_hash = {}
+  roster.each do |x, y|
+    nu_hash[x] = y.sort
+  end
+  nu_hash
+end
+end
+
 
 end
