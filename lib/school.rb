315ffe7543622a010f5770f00027015937be5eb8
Devin Wilson <craigvin84@yahoo.com>
2020-10-31 20:48:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
 # code here!
+class School
+
+
+roster => {}
+
+def add_student=(name, grade)
+  @name=(name)
+  @grade=(grade)
+  roster[grade] << name
+end
+
+def add_student
+  @name
+  @grade
+end
+
+end
